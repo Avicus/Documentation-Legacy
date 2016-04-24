2512a48f7c7dd26ce4c3637d6eb74decdf16484a
thekeenant <keenan@keenant.com>
2016-04-24 13:18:36 -0500

    Fix xml example in loadouts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.4.1'
 
 set :application, 'atlas.avicus.net'
 set :repo_url, 'git@github.com:Avicus/atlas.avicus.net.git'
