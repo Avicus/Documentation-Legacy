5c0d43a1dcaaa741a29cdc2cd6c0000fa8ff8ef9
Austin Mayes <bookofnathan@gmail.com>
2016-07-08 00:49:41 -0500

    Ignore logs, update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.1'
+lock '3.5.0'
 
 set :application, 'atlas.avicus.net'
 set :repo_url, 'git@github.com:Avicus/atlas.avicus.net.git'
